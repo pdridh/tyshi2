1a4577635239131f88572b38e765f51a285b73ca
imagine <91915381+ImaginePog@users.noreply.github.com>
2024-09-07 07:10:33 +0545

    Add back less "pure" shit cause quality of life

--- src/components/Animator.h
+++ src/components/Animator.h
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 #include "../utils/Vec2.h"
 
@@ -12,9 +13,9 @@ struct Animation
   // The rect to draw from the texture (source rect)
   SDL_Rect frameRect;
   // The x'th number of frame to start from (not in actual pixels but frames)
-  int startX;
+  int startX = 0;
   // The y'th number of frame to start from (not in actual pixels but frames)
-  int startY;
+  int startY = 0;
   // The number of frames that this animation has
   int frames;
   // The current frame that is being shown
@@ -29,9 +30,57 @@ struct Animation
 
 struct Animator
 {
-  Vec2 m_scale;
   SDL_Texture *m_sheet;
-  SDL_RendererFlip m_renderFlipped;
-  int m_currentAnimation;
   std::vector<Animation *> m_animations;
+  Vec2 m_scale;
+  int m_currentAnimation;
+  SDL_RendererFlip m_renderFlipped;
+
+  Animator(SDL_Texture *sheet, int nAnimations, Vec2 scale)
+      : m_sheet{sheet}, m_animations{nAnimations, nullptr},
+        m_scale{scale}, m_currentAnimation{0}, m_renderFlipped{SDL_FLIP_NONE}
+  {
+  }
+
+  void addAnimation(int id, int startX, int startY,
+                    int frames, int frameWidth,
+                    int frameHeight, double fps,
+                    double angle)
+  {
+    assert(isAssigned(id));
+
+    Animation *newAnimation = new Animation();
+    newAnimation->startX = startX;
+    newAnimation->startY = startY;
+    newAnimation->frameRect.x = startX * frameWidth;
+    newAnimation->frameRect.y = startY * frameHeight;
+    newAnimation->frameRect.w = frameWidth;
+    newAnimation->frameRect.h = frameHeight;
+
+    newAnimation->frames = frames;
+    newAnimation->currentFrame = 0;
+    newAnimation->frameTime = 0.0;
+    newAnimation->frameDuration = 1 / fps; // calculate duration of each animation from fps
+
+    newAnimation->angle = angle;
+
+    m_animations[id] = newAnimation;
+  }
+
+  bool isAssigned(int id)
+  {
+    Animation *found = m_animations[id];
+    return found;
+  }
+
+  void reset(int id)
+  {
+    assert(!isAssigned(id));
+
+    Animation *anim = m_animations[id];
+    anim->frameRect.x = anim->startX * anim->frameRect.w;
+    anim->frameRect.y = anim->startY * anim->frameRect.h;
+    anim->currentFrame = 0;
+    anim->frameTime = 0.0;
+  }
 };
