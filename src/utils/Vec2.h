7649ef0332e0bda88c566bdfcb65d8ae0bbd7acf
pdridh <91915381+pdridh@users.noreply.github.com>
2025-01-22 20:11:25 +0545

    Change double to float for easy SDL converts

--- src/utils/Vec2.h
+++ src/utils/Vec2.h
@@ -6,11 +6,10 @@
 class Vec2
 {
 public:
-  double x = 0.0;
-  double y = 0.0;
+  float x;
+  float y;
 
-  Vec2() {}
-  Vec2(double x_arg, double y_arg) : x(x_arg), y(y_arg) {}
+  Vec2(float x_arg = 0.0, float y_arg = 0.0) : x(x_arg), y(y_arg) {}
 
   // Get zero vector
   inline static Vec2 ZERO() { return Vec2(0, 0); }
@@ -20,7 +19,7 @@ public:
   {
     if (x || y)
     {
-      double mag = magnitude();
+      float mag = magnitude();
 
       x /= mag;
       y /= mag;
@@ -32,19 +31,19 @@ public:
   inline Vec2 getNormalized() const { return Vec2(*this).normalize(); }
 
   // The magnitude of this vector
-  inline double magnitude() const { return sqrt((x * x) + (y * y)); }
+  inline float magnitude() const { return sqrt((x * x) + (y * y)); }
 
   // Magnitude squared for faster calculations (no sqrt())
-  inline double magnitudeSquared() const { return (x * x) + (y * y); }
+  inline float magnitudeSquared() const { return (x * x) + (y * y); }
 
   // Distance from this vector to a vector
-  inline double distanceTo(const Vec2 &right) const { return (*this - right).magnitude(); }
+  inline float distanceTo(const Vec2 &right) const { return (*this - right).magnitude(); }
 
   // Distance distance from this vector to a vector
-  inline double distanceToSquared(const Vec2 &right) const { return (*this - right).magnitudeSquared(); }
+  inline float distanceToSquared(const Vec2 &right) const { return (*this - right).magnitudeSquared(); }
 
   // Scale the vector by a factor
-  inline Vec2 &scale(double factor)
+  inline Vec2 &scale(float factor)
   {
     x *= factor;
     y *= factor;
@@ -53,7 +52,7 @@ public:
   };
 
   // Get scaled form without modifying the vector
-  inline Vec2 getScaled(double factor) const { return Vec2(x * factor, y * factor); }
+  inline Vec2 getScaled(float factor) const { return Vec2(x * factor, y * factor); }
 
   // Operations
   inline Vec2 operator+(Vec2 const &right) const { return Vec2(x + right.x, y + right.y); }
