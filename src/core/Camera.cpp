952147bf4643d50e9f2e4f92c1f306077b9f9a6a
pdridh <91915381+pdridh@users.noreply.github.com>
2025-01-23 18:35:33 +0545

    Split functions into .cpp file

---
+++ src/core/Camera.cpp
@@ -0,0 +1,48 @@
+#include "Camera.h"
+
+Camera::Camera(SDL_Renderer *renderer, float width, float height)
+    : m_renderer{renderer}, m_width{width}, m_height{height}
+{
+}
+
+void Camera::centerOn(Vec2 centerPos)
+{
+  m_center = centerPos - Vec2(m_width / 2, m_height / 2);
+}
+
+Vec2 Camera::screenToWorld(Vec2 screenPos)
+{
+  return screenPos + m_center;
+}
+
+Vec2 Camera::worldToScreen(Vec2 worldPos)
+{
+  return worldPos - m_center;
+}
+
+void Camera::drawRect(Vec2 worldPos, float width, float height, Color color)
+{
+  SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
+
+  // Everything is centered
+  Vec2 finalPos = worldToScreen(Vec2(worldPos.x - width / 2, worldPos.y - height / 2));
+  SDL_FRect rect = {finalPos.x,
+                    finalPos.y,
+                    width,
+                    height};
+
+  SDL_RenderDrawRectF(m_renderer, &rect);
+}
+
+void Camera::drawRect(float worldX, float worldY, float width, float height, Color color)
+{
+  drawRect(Vec2(worldX, worldY), width, height, color);
+}
+
+void Camera::drawTexture(SDL_Texture *texture, SDL_Rect &srcRect, Vec2 worldPos, float width, float height)
+{
+  Vec2 screenPos = worldToScreen(worldPos);
+  SDL_FRect dstRect = {screenPos.x, screenPos.y, width, height};
+
+  SDL_RenderCopyExF(m_renderer, texture, &srcRect, &dstRect, 0, NULL, SDL_FLIP_NONE);
+}
--- src/core/Camera.h
+++ src/core/Camera.h
@@ -21,41 +21,11 @@ private:
   float m_height;
 
 public:
-  Camera(SDL_Renderer *renderer, float width, float height)
-      : m_renderer{renderer}, m_width{width}, m_height{height}
-  {
-  }
-
-  void centerOn(Vec2 centerPos)
-  {
-    m_center = centerPos - Vec2(m_width / 2, m_height / 2);
-  }
-
-  Vec2 screenToWorld(Vec2 screenPos)
-  {
-    return screenPos + m_center;
-  }
-
-  Vec2 worldToScreen(Vec2 worldPos)
-  {
-    return worldPos - m_center;
-  }
-
-  void drawRect(Vec2 worldPos, float width, float height, Color color)
-  {
-    SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
-
-    // Everything is centered
-    Vec2 finalPos = worldToScreen(Vec2(worldPos.x - width / 2, worldPos.y - height / 2));
-    SDL_FRect rect = {finalPos.x,
-                      finalPos.y,
-                      width,
-                      height};
-
-    SDL_RenderDrawRectF(m_renderer, &rect);
-  }
-  void drawRect(float worldX, float worldY, float width, float height, Color color)
-  {
-    drawRect(Vec2(worldX, worldY), width, height, color);
-  }
+  Camera(SDL_Renderer *renderer, float width, float height);
+  void centerOn(Vec2 centerPos);
+  Vec2 screenToWorld(Vec2 screenPos);
+  Vec2 worldToScreen(Vec2 worldPos);
+  void drawRect(Vec2 worldPos, float width, float height, Color color);
+  void drawRect(float worldX, float worldY, float width, float height, Color color);
+  void drawTexture(SDL_Texture *texture, SDL_Rect &srcRect, Vec2 worldPos, float width, float height);
 };
