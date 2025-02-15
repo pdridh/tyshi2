fc22818435937610e574b258e972904d975b051f
pdridh <91915381+pdridh@users.noreply.github.com>
2025-02-15 17:17:31 +0545

    Forgot this

--- CMake/mingw-toolchain.cmake
+++ CMake/mingw-toolchain.cmake
@@ -6,7 +6,7 @@ set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 
 # Set the Windows path for finding libraries
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32 /usr/local/mingw)
+set(CMAKE_FIND_ROOT_PATH /usr/local/x86_64-w64-mingw32)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
